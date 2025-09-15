fcb891893b6066fb9ef8e6b5ca69dee64acae1a2
Danilson Bertil <danilsonbertil.13@gmail.com>
2025-09-15 20:44:13 +0200

    Configurando os diretorios

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,24 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const incrementar = () => {
+    setCount(count + 1);
+  };
 
+    const decrementar = () => {
+    setCount(count - 1);
+  };
   return (
    <>
   <h1 class="text-3xl font-bold underline">
-    Hello world!
+    Hello world! {count}
   </h1>
-
+<button onClick={incrementar}>
+  Incrementar
+</button>
+<button onClick={decrementar}>
+  Decrementar
+</button>
     </>
   )
 }
